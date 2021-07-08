6b94746700adaab6ae7de57777c523cd8f2a0273
Omar Alazaizeh <omx302@gmail.com>
2021-07-08 03:42:56 +0300

    CC08

--- javascript/code-challenges/challenges-08.test.js
+++ javascript/code-challenges/challenges-08.test.js
@@ -141,51 +141,52 @@ CHALLENGE 7 - Stretch Goal
 
 You have created a game application and begin by asking users an easy question: In which month is Halloween?
 
-Write a function named matchMonth which uses a regular expression pattern to match any of these inputs: October, Oct, october, oct
+Write a function named matchMonth which uses a regular expression pattern to match any of these 
+inputs: October, Oct, october, oct
 
 If the user enters any of these four inputs, return true. For any other input, return false.
 
 Do not use the vertical bar (pipe) in your pattern.
 ------------------------------------------------------------------------------------------------ */
 
-const matchMonth = (input) => {
-  // Solution code here...
-};
+const matchMonth = (input) => /\b([oO]ct)(ober)?\b/g.test(input);
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 8 - Stretch Goal
 
-Write a function named noPunctuation that contains a regular expression pattern to find all of the words that contain a space immediately at the end of the word. Return an array of all such words, still containing the space at the end.
+Write a function named noPunctuation that contains a regular expression pattern to 
+find all of the words that contain a space immediately at the end of the word.
+ Return an array of all such words, still containing the space at the end.
 
-For example, if given the string "Hello, and have a wonderful day!", the word "Hello, " would not be returned because it is immediately followed by a comma. The word "day!" would not be returned because it is immediately followed by an exclamation point.
+For example, if given the string "Hello, and have a wonderful day!",
+ the word "Hello, " would not be returned because it is immediately followed by a comma.
+  The word "day!" would not be returned because it is immediately followed by an exclamation point.
 
 The expected output of "Hello, and have a wonderful day!" is ["and ", "have ", "a ", "wonderful "].
 ------------------------------------------------------------------------------------------------ */
 
-const noPunctuation = (str) => {
-  // Solution code here...
-};
+const noPunctuation = (str) => str.match(/\b(\w)*\s/g);
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 9 - Stretch Goal
 
 You want to teach a friend how to play hangman and want to show them using a partially complete puzzle.
 
-Write a function named hangman which uses the replace method to remove all of the vowels (a, e, i, o, u) from the hangman string, regardless of capitalization, and replace them with an underscore.
+Write a function named hangman which uses the replace method to remove all of the vowels
+ (a, e, i, o, u) from the hangman string, regardless of capitalization, and replace them with an underscore.
 
 The function should return a string containing the consonants in their original positions and underscores where the vowels were previously located.
 
 For example, 'Welcome to Code 301!' will return 'W_lc_m_ t_ C_d_ 301!'.
 ------------------------------------------------------------------------------------------------ */
 
-let hangman = (str) => {
-  // Solution code here...
-};
-
+let hangman = (str) => str.replace(/[aeiou]/gi, "_");
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 10 - Stretch Goal
 
-Write a function named findShells that takes in the string below and uses a regular expression pattern to find all instances of the following words: "sells", "shells", "seashells".
+Write a function named findShells that takes in the string 
+below and uses a regular expression pattern to find all instances 
+of the following words: "sells", "shells", "seashells".
 
 Do not use the vertical bar (pipe) character.
 
@@ -195,9 +196,7 @@ Hint: All of these words end with the letters "ells".
 const seashells =
   "She sells seashells by the seashore. The shells she sells are surely seashells. So if she sells shells on the seashore, I'm sure she sells seashore shells.";
 
-const findShells = (str) => {
-  // Solution code here...
-};
+const findShells = (str) => str.match(/\w*lls/g);
 
 /* ------------------------------------------------------------------------------------------------
 TESTS
@@ -339,7 +338,7 @@ describe("Testing challenge 6", () => {
   });
 });
 
-xdescribe("Testing challenge 7", () => {
+describe("Testing challenge 7", () => {
   test("It should match any of the acceptable inputs", () => {
     expect(matchMonth("Oct")).toBeTruthy();
     expect(matchMonth("oct")).toBeTruthy();
@@ -357,7 +356,7 @@ xdescribe("Testing challenge 7", () => {
   });
 });
 
-xdescribe("Testing challenge 8", () => {
+describe("Testing challenge 8", () => {
   const lorem =
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras lacinia vel massa sed egestas. Nunc faucibus iaculis elit, a scelerisque enim condimentum sed. Aenean ac scelerisque sem, et pharetra diam.";
 
@@ -456,7 +455,7 @@ xdescribe("Testing challenge 8", () => {
   });
 });
 
-xdescribe("Testing challenge 9", () => {
+describe("Testing challenge 9", () => {
   let startString =
     "This is a regex challenge. We are trying to create a hangman phrase where all of the vowels are missing!";
 
@@ -476,7 +475,7 @@ xdescribe("Testing challenge 9", () => {
   });
 });
 
-xdescribe("Testing challenge 10", () => {
+describe("Testing challenge 10", () => {
   test('It should return an array of instances of "sells", shells", and "seashells"', () => {
     expect(findShells(seashells)).toStrictEqual([
       "sells",
